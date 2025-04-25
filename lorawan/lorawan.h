39c8f715ebff7a54bf3864169f66b837a44bb179
Aidan <acm289@cornell.edu>
2025-04-25 14:33:53 -0400

    Add template LoRaWAN code

---
+++ lorawan/lorawan.h
@@ -0,0 +1,25 @@
+// =======================================================================
+// lorawan.h
+// =======================================================================
+// Declarations of our LoRaWAN interface
+
+#ifndef LORAWAN_LORAWAN_H
+#define LORAWAN_LORAWAN_H
+
+#include "lorawan/lorawan_config.h"
+#include <cstdint>
+
+class LoRaWAN {
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // Public Accessor Functions
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  LoRaWAN();
+
+  // Return whether joining is successful
+  bool try_join();
+
+  // Return whether sending the message is successful
+  bool try_send( const uint8_t* data, uint8_t data_len );
+};
+
+#endif  // LORAWAN_LORAWAN_H
